1eed8a198a7103de71b4785d2686783f58e8aa11
geekboysupreme <shuvam.manna@gmail.com>
2021-01-17 13:15:56 +0530

    Linking functions

--- Frontend/assets/scripts.js
+++ Frontend/assets/scripts.js
@@ -15,3 +15,30 @@ function update_disease(q) {
   disease = q;
 }
 
+function update_smoke(w) {
+  smoke = w;
+}
+
+function update_response(e) {
+  response = e;
+}
+
+function update_family(p) {
+  family = p;
+}
+
+function update_interaction(z) {
+  interaction = z;
+}
+
+function update_age(den) {
+  age = document.getElementById(den).value;
+}
+
+function update_pin(pinvalue) {
+  pincode = document.getElementById(pinvalue).value;
+}
+
+function update_spotify(sp_link) {
+  spotify_link = document.getElementById(sp_link).value;
+}
